0be2367b66229a00c1fbfc16cad1dd6756f6afa7
Gustavo Tonietto <tonietto@gmail.com>
2018-03-08 14:50:06 -0300

    [Refactor] Refactor updater

--- src/updater/index.js
+++ src/updater/index.js
@@ -2,16 +2,12 @@
  * Update something from original to final if forbid doesn't pass
  *
  * @param  {String}   original  The original value
- * @param  {Boolean}  check     An externally provided checking to define if the update should really occur
+ * @param  {Boolean}  forbid    An externally provided checking to define if the update should really occur
  * @param  {String}   final     The final value, to which the orinal should be updated, in case forbid is false
  * @return {String}             The resulting value, either original or final
  */
 
-const updater = (original, forbid = false, final) => {
-  if (!forbid) {
-    return final;
-  }
-  return original;
-};
+const updater = (original, forbid = false, final) =>
+  !forbid ? final : original;
 
 module.exports = updater;
