77371483a333be87d74d08040a4828f995d66a94
Gustavo Tonietto <tonietto@gmail.com>
2018-03-08 15:08:53 -0300

    [Tests] Update sanitizer key tests

--- src/sanitizer/key/key.test.js
+++ src/sanitizer/key/key.test.js
@@ -6,24 +6,25 @@ const sanitizer = require('./key');
 // let skusCounter = Math.floor(Math.random() * 999) + 0;
 // const mockSanitize = jest.fn();
 
-// define default product
-const product = {
+// define default list
+const list = () => ({
   id: '',
   link: '',
   name: '',
-};
+});
 
 describe('sanitize id', () => {
   test('should change to sku', () => {
     let key = 'id';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, list());
     expect(newKey).toBe('sku');
   });
 
   test('should not change if an sku already exists', () => {
-    product.sku = '#123';
+    let newList = list();
+    newList.sku = '#123';
     let key = 'id';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, newList);
     expect(newKey).toBe('id');
   });
 });
@@ -31,14 +32,15 @@ describe('sanitize id', () => {
 describe('sanitize link', () => {
   test('should change to url', () => {
     let key = 'link';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, list());
     expect(newKey).toBe('url');
   });
 
   test('should not change if an url already exists', () => {
-    product.url = '#123';
+    let newList = list();
+    newList.url = '#123';
     let key = 'link';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, newList);
     expect(newKey).toBe('link');
   });
 });
@@ -46,14 +48,55 @@ describe('sanitize link', () => {
 describe('sanitize title', () => {
   test('should change to name', () => {
     let key = 'title';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, list());
     expect(newKey).toBe('name');
   });
 
   test('should not change if a name already exists', () => {
-    product.name = '#123';
+    let newList = list();
+    newList.name = '#123';
     let key = 'title';
-    const newKey = sanitizer(key, product);
+    const newKey = sanitizer(key, newList);
     expect(newKey).toBe('title');
+  });
+});
+
+describe('sanitize additional_image_link', () => {
+  test('should change to images', () => {
+    let key = 'additional_image_link';
+    const newKey = sanitizer(key, list());
+    expect(newKey).toBe('images');
+  });
+
+  test('should not change if image_link already exists', () => {
+    let newList = list();
+    newList.image_link = '#123';
+    let key = 'additional_image_link';
+    const newKey = sanitizer(key, newList);
+    expect(newKey).toBe('additional_image_link');
+  });
+
+  test('should not change if images already exists', () => {
+    let newList = list();
+    newList.images = '#123';
+    let key = 'additional_image_link';
+    const newKey = sanitizer(key, newList);
+    expect(newKey).toBe('additional_image_link');
+  });
+});
+
+describe('sanitize google_product_category', () => {
+  test('should change to categories', () => {
+    let key = 'google_product_category';
+    const newKey = sanitizer(key, list());
+    expect(newKey).toBe('categories');
+  });
+
+  test('should not change if categories already exists', () => {
+    let newList = list();
+    newList.categories = '#123';
+    let key = 'google_product_category';
+    const newKey = sanitizer(key, newList);
+    expect(newKey).toBe('google_product_category');
   });
 });
